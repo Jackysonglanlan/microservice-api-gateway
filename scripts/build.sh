fd0972bde7e46691efc3b97b468665e42e2ce726
Jacky.Song <jackysonglanlan@gmail.com>
2017-10-02 14:09:38 +0800

    [refine] build script\n [add] npm hook predev-start-lua

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,17 +4,44 @@
 set -euo pipefail
 trap "echo 'error: Script failed: see failed command above'" ERR
 
-mac(){
+use_red_green_echo() {
+  prefix="$1"
+  red() {
+    echo "$(tput bold)$(tput setaf 1)[$prefix] $*$(tput sgr0)";
+  }
+  
+  green() {
+    echo "$(tput bold)$(tput setaf 2)[$prefix] $*$(tput sgr0)";
+  }
+  
+  yellow() {
+    echo "$(tput bold)$(tput setaf 3)[$prefix] $*$(tput sgr0)";
+  }
+}
+use_red_green_echo 'Build'
+
+
+ALL_LIBS_BUILT_MARK_FILE='./all-libs-built.log'
+
+_on_mac(){
   if [[ $(uname -a) == *Darwin* ]]; then
+    yellow "[ON MAC] $@"
     "$@"
   fi
 }
 
-linux(){
+_on_linux(){
   if [[ $(uname -a) == Linux* ]]; then
+    yellow "[ON LINUX] $@"
     "$@"
   fi
 }
+
+_markAllLibsAreBuilt(){
+  touch $ALL_LIBS_BUILT_MARK_FILE
+}
+
+###### build all the C libs
 
 # $1: LIB_OPTION
 build_lfs(){
@@ -22,8 +49,21 @@ build_lfs(){
   make PREFIX=$LUA_HOME LIB_OPTION="$1" && make test && make install && make clean
 }
 
+# ...
 
-# main
+main(){
+  if [[ -f $ALL_LIBS_BUILT_MARK_FILE ]]; then
+    yellow 'All libs are built, no need to build...'
+    return
+  fi
+  
+  yellow 'Start building C libs...'
+  
+  local current=$PWD
+  _on_mac build_lfs "-bundle -undefined dynamic_lookup"
+  _on_linux build_lfs "-shared"
+  cd $current
+  _markAllLibsAreBuilt
+}
 
-mac build_lfs "-bundle -undefined dynamic_lookup"
-linux build_lfs "-shared"
+main
