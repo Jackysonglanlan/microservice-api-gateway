40e0a43057a2b1d5743c1a0d1f2832b4fec6722e
Jacky.Song <jackysonglanlan@gmail.com>
2017-10-17 15:43:55 +0800

    [add] npm script "test" [refine] busted launch process

--- test/lua-demo/demo.lua
+++ test/lua-demo/demo.lua
@@ -32,63 +32,6 @@ It is therefore highly recommended to always declare such within an appropriate 
 
 -- ///////////////
 
-local function testLuahs()
-  local hs = require('luahs') -- need gcc to have "GLIBCXX_3.4.20", or it will fail
-  db = hs.compile {
-    expression = 'aaa', 
-    mode = hs.compile_mode.HS_MODE_BLOCK, 
-  }
-  scratch = db:makeScratch()
-  
-  hits = db:scan('aaa', scratch)
-  
-  utils.log(hits)
-end
--- testLuahs()
-
-local function testHttpipe(url)
-  local httpipe = require('http.httpipe')
-  local hp, err = httpipe:new(100)
-  
-  local ok, err = hp:connect(url, 80)
-  if not ok then
-    utils.log("failed to request: ", err)
-    return
-  end
-  local ok, err = hp:send_request({method = "GET" })
-  
-  local res = {}
-  hp:read_response({body_filter = function(data)
-      table.insert(res, data)
-  end})
-  
-  utils.log(table.concat(res, ''))
-end
--- testHttpipe('www.baidu.com')
-
-local function performHttpRequest()
-  local uv = require("lluv")
-  local curl = require "lluv.curl"
-  local loop = uv.default_loop()
-  
-  local function writeToResp(data)
-    utils.log(data)
-  end
-  
-  local easy = curl.easy({
-    url = "http://www.baidu.com";
-    writefunction = writeToResp
-  })
-  
-  local multi = curl.multi()
-  multi:add_handle(easy, function(easy, err)
-      utils.log("Done:", err or easy:getinfo_response_code())
-      uv.stop(loop) -- stop the loop, or it will block the nginx thread
-  end)
-  uv.run(loop)
-end
--- performHttpRequest()
-
 local function md5AndCJSON()
   local resty_md5 = require "resty.md5"
   local string = require "resty.string"
@@ -108,51 +51,6 @@ local function md5AndCJSON()
 end
 md5AndCJSON()
 
-local function testLFS(path)
-  local lfs = require("lfs")
-  
-  for file in lfs.dir(path) do
-    if file ~= "." and file ~= ".." then
-      local f = path .. '/' .. file
-      utils.log(f)
-      local attr = lfs.attributes (f)
-      assert (type(attr) == "table")
-      if attr.mode == "directory" then
-        testLFS (f)
-        -- else
-        --   for name, value in pairs(attr) do
-        --     utils.log(name, value)
-        --   end
-      end
-    end
-  end
-end
--- testLFS('lua/libs/.prebuild')
-
-local function unittest()
-  local iresty_test    = require ("test.resty_test")(ngx)
-  local tb = iresty_test.new({unit_name = "example"})
-  
-  function tb:init(  )
-    self:log("init complete")
-  end
-  
-  function tb:test_00001(  )
-    error("invalid input")
-  end
-  
-  function tb:atest_00002()
-    self:log("never be called")
-  end
-  
-  function tb:test_00003(  )
-    self:log("ok")
-  end
-  
-  -- units test
-  tb:run()
-end
--- unittest()
 
 local function testTamale()
   local tamale =  require "tamale.tamale"
@@ -161,7 +59,7 @@ local function testTamale()
     { {"foo", 1, {} },      "one" }, 
     { 10,                   function() return "two" end}, 
     { {"bar", 10, 100},     "three" }, 
-    { {"baz", V"X" },       V"X" },                                         -- V"X" is a variable
+    { {"baz", V"X" },       V"X" },                                            -- V"X" is a variable
     { {"add", V"X", V"Y"},  function(cs) return cs.X + cs.Y end }, 
   }
   
