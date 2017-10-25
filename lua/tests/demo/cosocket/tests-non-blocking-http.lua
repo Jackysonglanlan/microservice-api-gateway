aa868a43ae50bea0975fccbfd127ae8b5d5593f7
Jacky.Song <jackysonglanlan@gmail.com>
2017-10-25 18:23:24 +0800

    [add] more demo tests

---
+++ lua/tests/demo/cosocket/tests-non-blocking-http.lua
@@ -0,0 +1,29 @@
+
+describe("cosocket", function()
+    -- see https://www.openresty.com.cn/pra_ngx_lua_whats_cosocket.html
+    it("should perform non-blocking socket", function()
+        local sock = ngx.socket.tcp()
+        local url = 'www.baidu.com'
+        local ok, err = sock:connect(url, 80)
+        if not ok then
+          print("failed to connect to baidu: ", err)
+          return
+        end
+        
+        local req_data = 'GET / HTTP/1.1\r\nHost: ' .. url .. '\r\n\r\n'
+        local bytes, err = sock:send(req_data)
+        if err then
+          print("failed to send to baidu: ", err)
+          return
+        end
+        
+        local data, err, partial = sock:receive()
+        if err then
+          print("failed to recieve to baidu: ", err)
+          return
+        end
+        
+        sock:close()
+        print('successfully talk to ' .. url .. '! response first line: ', data)
+      end)
+end)
